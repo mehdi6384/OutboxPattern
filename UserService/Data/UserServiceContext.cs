645b5314dc610184e197868dc134554466763d14
sm.hosseini <sm.hosseini@emofid.com>
2022-04-24 17:57:56 +0430

    Feat: User service completed

---
+++ UserService/Data/UserServiceContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace UserService.Data;
+
+public class UserServiceContext : DbContext
+{
+    public UserServiceContext(DbContextOptions<UserServiceContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<Entities.User> User { get; set; }
+}
--- UserService/Program.cs
+++ UserService/Program.cs
@@ -1,17 +1,31 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using UserService.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddControllers();
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddDbContext<UserServiceContext>(options =>
+         options.UseSqlite(@"Data Source=user.db"));
+
 var app = builder.Build();
+//var dbContext = new UserServiceContext(new DbContextOptions<UserServiceContext>());
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+    //dbContext.Database.EnsureCreated();
 }
+
+app.UseAuthorization();
+
+app.MapControllers();
 
 app.Run();
